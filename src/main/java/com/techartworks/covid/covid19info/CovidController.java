1493e22bef9057c233216cc192537cd40ef0e01b
bhatnagarm <mitul_manik@yahoo.com>
2020-12-23 22:33:42 +1000

    fix(develop): remove unused headers.

--- src/main/java/com/techartworks/covid/covid19info/CovidController.java
+++ src/main/java/com/techartworks/covid/covid19info/CovidController.java
@@ -4,7 +4,6 @@ import com.techartworks.covid.covid19info.domain.model.CountryInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
